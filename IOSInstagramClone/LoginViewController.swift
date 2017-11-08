9acd70197c22b9f88fded66dfd0b5f2828f7a2de
ercanpinar <ercanpinarr@gmail.com>
2017-11-09 00:54:32 +1100

    Added LoginPage and Edited storyboard

---
+++ IOSInstagramClone/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  IOSInstagramClone
+//
+//  Created by Ercan Pinar on 9/11/17.
+//  Copyright © 2017 Ercan PINAR. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func signInClicked(_ sender: Any) {
+    }
+
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
+    
+    
+}
